2e3005fd408f5fe0b781192e9337babd3d6d98d6
Robin Mi <si.mi5689@gmail.com>
2017-07-15 12:20:33 -0700

    I think local login functions are now working.

--- app/components/Main.js
+++ app/components/Main.js
@@ -116,7 +116,7 @@ var Main = React.createClass({
           <div className = "jumbotron">
 
             <Route exact path="/" component={() => <ArticlesChildren articles={this.state.articles}/>}/>
-            <Route path="/login" component={() => <LoginPage loginLocalUser={this.loginLocalUser} createAccount={this.createAccount} />} />
+            <Route path="/login" component={() => <LoginPage loginLocalUser={(user) => this.loginLocalUser(user)} createAccount={(user) => this.createAccount(user)} />} />
             <Route path="/profile" component={() => <ProfilePage user={this.state.user}/>}/>
           </div> {/* end jumbotron */}
 
