5e2d128760986c681cfbdb778a9c4facbfe35732
Robin Mi <si.mi5689@gmail.com>
2017-07-17 21:11:19 -0700

    fixed an error for logout

--- app/components/children/ProfilePage.js
+++ app/components/children/ProfilePage.js
@@ -14,10 +14,29 @@ export default class ProfilePage extends React.Component {
 
   constructor(props) {
     super(props);
+
+    
   };
 
-
+  componentDidMount()
+  {
+    console.log(JSON.stringify(this.props.user, null, 2));
+  }
 //TODO: add a list of toggles that keeps track of what categories that the user wants to experience. 
+  handleToggle(event, state)
+  {
+    console.log("toggled: " + state);
+    this.props.preferenceToggle(state, event.target.value);
+  }
+
+  handleSavePreferences(event)
+  {
+    event.preventDefault();
+    console.log("saving preferences");
+  }
+
+// <Toggle onToggle={(event) => this.handleChange(event, 'encode')} style={styles.toggle} label="Encode" labelPosition="right" />
+// handleChange(event, state) { console.log(state, event.target.value); this.setState({ [state]: event.target.value }); }
 
   render() {
       return (
@@ -30,26 +49,46 @@ export default class ProfilePage extends React.Component {
         <Divider />
         <List>
             <Subheader>Your Preferences</Subheader>
-            <ListItem primaryText="World News" rightToggle={<Toggle />} />
+            
+            <ListItem primaryText="World News" rightToggle={<Toggle 
+                defaultToggled={this.props.user['world_news']}
+                onToggle={(event) => this.handleToggle(event, 'world_news')}
+                />} />
             <hr/>
-            <ListItem primaryText="Politics" rightToggle={<Toggle />} />
+            <ListItem primaryText="Politics" rightToggle={<Toggle 
+              defaultToggled={this.props.user['politics']}
+              onToggle={(event) => this.handleToggle(event, 'politics')}/>} />
             <hr/>
-            <ListItem primaryText="Science" rightToggle={<Toggle />} />
+            <ListItem primaryText="Science" rightToggle={<Toggle 
+              defaultToggled={this.props.user['science']}
+              onToggle={(event) => this.handleToggle(event, 'science')}/>} />
             <hr/>
-            <ListItem primaryText="Movies" rightToggle={<Toggle />} />
+            <ListItem primaryText="Movies" rightToggle={<Toggle 
+              defaultToggled={this.props.user['movies']}
+              onToggle={(event) => this.handleToggle(event, 'movies')}/>} />
             <hr/>
-            <ListItem primaryText="Business" rightToggle={<Toggle />} />
+            <ListItem primaryText="Business" rightToggle={<Toggle 
+              defaultToggled={this.props.user['business']}
+              onToggle={(event) => this.handleToggle(event, 'business')}/>} />
             <hr/>
-            <ListItem primaryText="Arts" rightToggle={<Toggle />} />
+            <ListItem primaryText="Arts" rightToggle={<Toggle 
+              defaultToggled={this.props.user['arts']}
+              onToggle={(event) => this.handleToggle(event, 'arts')}/>} />
             <hr/>
-            <ListItem primaryText="Travel" rightToggle={<Toggle />} />
+            <ListItem primaryText="Travel" rightToggle={<Toggle 
+              defaultToggled={this.props.user['travel']}
+              onToggle={(event) => this.handleToggle(event, 'travel')}/>} />
             <hr/>
-            <ListItem primaryText="Sports" rightToggle={<Toggle />} />
+            <ListItem primaryText="Sports" rightToggle={<Toggle 
+              defaultToggled={this.props.user['sports']}
+              onToggle={(event) => this.handleToggle(event, 'sports')}/>} />
             <hr/>
-            <ListItem primaryText="Fashion" rightToggle={<Toggle />} />
+            <ListItem primaryText="Fashion" rightToggle={<Toggle 
+              defaultToggled={this.props.user['fashion']}
+              onToggle={(event) => this.handleToggle(event, 'fashion')}/>} />
             <hr/>
         </List>
-        <RaisedButton label="Save Preferences" primary={true} style={style}/>
+        <RaisedButton label="Save Preferences" primary={true} style={style} onClick={(event) => this.handleSavePreferences(event)}/>
     </div>
       )
   }
