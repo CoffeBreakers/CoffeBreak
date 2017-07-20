40b3a914efd1f11717be9cd10c80894ac20c6db7
Robin Mi <si.mi5689@gmail.com>
2017-07-19 19:11:57 -0700

    saving user preferences now works.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,12 +5,12 @@ module.exports = function(app, db)
     app.post('/user/preferences', function(req, res)
     {
         console.log(JSON.stringify(req.body, null, 2));
-        User.findOneAndUpdate({"_id": req.body.id}, {"preferences": req.body.preferences})
+        User.findOneAndUpdate({"_id": req.body._id}, req.body)
         .exec(function(err, doc)
         {
-            if(error)
+            if(err)
             {
-            console.log(error);
+            console.log(err);
             }
             else
             {
